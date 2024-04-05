4b7fb3b6a3ae2b37230b8b7590129ae2d122f88f
Shai Zambrovski <shaike.zam@gmail.com>
2024-04-05 23:54:04 +0300

    ui

---
+++ ui-service/public/orders.js
@@ -0,0 +1,30 @@
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('app', () => ({
+            orders: [],
+            currentPage: 1,
+            pageSize: 5,
+            search: '',
+            filteredOrders() {
+                if (!this.search.trim()) {
+                    return this.orders;
+                }
+                return this.orders.filter(product =>
+                    product.description.toLowerCase().includes(this.search.toLowerCase()) ||
+                    product.text.toLowerCase().includes(this.search.toLowerCase())
+                );
+            },
+            init() {
+                this.fetchOrders();
+            },
+            fetchOrders() {
+                fetch('/web/api/orders')
+                    .then(response => response.json())
+                    .then(data => {
+                        this.orders = data;
+                    })
+                    .catch(error => {
+                        console.error('Error fetching orders:', error);
+                    });
+            }
+        }));
+    });
